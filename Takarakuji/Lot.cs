4a5b6bf9cdbac29fc5a0da508c3bbe209b990812
Takatoshi Matsumoto <toqoz403@gmail.com>
2016-10-08 01:24:36 +0900

    first version

---
+++ Takarakuji/Lot.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+namespace Takarakuji
+{
+    public class Lot<T>
+    {
+        private List<T> xs = new List<T>();
+        private Xorshift random = new Xorshift();
+
+        public void Add(T x, int count = 1)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                xs.Add(x);
+            }
+        }
+
+        public void Remove(T x)
+        {
+            xs.RemoveAll(_x => EqualityComparer<T>.Default.Equals(x, _x));
+        }
+
+        public int Total()
+        {
+            return xs.Count;
+        }
+
+        public T Draw(bool removeIt = true)
+        {
+            var idx = random.Range(0, (uint)xs.Count);
+            var x = xs[(int)idx];
+            if (removeIt)
+            {
+                xs.RemoveAt((int)idx);
+            }
+            return x;
+        }
+    }
+
+    public class Xorshift
+    {
+        private uint x = 123456789;
+        private uint y = 362436069;
+        private uint z = 521288629;
+        private uint w = 88675123;
+
+        public void Seed(uint x, uint y, uint z, uint w)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        public uint Next()
+        {
+            uint t = x ^ (x << 11);
+            x = y;
+            y = z;
+            z = w;
+            w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+            return w;
+        }
+
+        public uint Range(uint min, uint max)
+        {
+            return min + Next() % (max - min);
+        }
+    }
+}
---
+++ TakarakujiTest/LotTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using Takarakuji;
+
+namespace TakarakujiTest
+{
+    [TestFixture]
+    public class XorshiftTest
+    {
+        [Test()]
+        [TestCase((uint)1, (uint)2)]
+        [TestCase((uint)1, (uint)500)]
+        [TestCase((uint)200, (uint)202)]
+        [TestCase((uint)2800, (uint)3000)]
+        public void TestEval(uint min, uint max)
+        {
+            var random = new Xorshift();
+            for (var i = 0; i <= 100; i++)
+            {
+                Assert.GreaterOrEqual(random.Range(min, max), min);
+                Assert.Less(random.Range(min, max), max);
+            }
+        }
+    }
+}
+
